91d7c382c72e2c2afaa19206fd37c015346e4c46
juan <juanan5febrero@gmail.com>
2021-05-06 12:21:20 +0200

    Final
    Nuevo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Create a "close" button and append it to each list item
+
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -9,7 +9,7 @@ for (i = 0; i < myNodelist.length; i++) {
     myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -19,7 +19,7 @@ for (i = 0; i < close.length; i++) {
     }
 }
 
-// Add a "checked" symbol when clicking on a list item
+
 var list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
     if (ev.target.tagName === 'LI') {
@@ -27,13 +27,20 @@ list.addEventListener('click', function (ev) {
     }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
+function listaFiltradaPrioridades(pPrioridad) {
+
+    const listaDePrioridades = listaDeElementos.filter(prioridadBuscada => prioridadBuscada.prioridad === pPrioridad);
+    return listaDePrioridades;
+}
+
+
 function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("miInput").value;
-    var t = document.createTextNode(inputValue);
+    var t = document.createTextNode(inputValue, prioridad);
+    var prioridad = document.getElementsByClassName('prioridades');
     li.appendChild(t);
-    if (inputValue === '') {
+    if (inputValue === '' || prioridad === '') {
         alert("Tienes que escribir algo!");
     } else {
         document.getElementById("ulTareas").appendChild(li);
@@ -41,7 +48,7 @@ function newElement() {
     document.getElementById("miInput").value = "";
 
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("Eliminar");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
